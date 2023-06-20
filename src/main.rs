a4ecab42861b61a03f320f62604a97493edb840a
yuanwai <lxdhzzj@gmail.com>
2023-06-20 14:28:08 +0800

    add post handler

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,44 @@
 use axum::{
-    routing::get,
-    Router, Json,
+    routing::{post,get},
+    Router
 };
+use serde::Deserialize;
+use std::{fs::OpenOptions,io::Write};
 
-use serde::Serialize;
+#[derive(Deserialize)]
+struct Log {
+    time: String,
+    client_ip: String,
+    log_content: String,
+}
 
-#[derive(Serialize)]
-pub struct User{
-    pub id:i32,
-    pub username:String,
-    pub age:i32,
-} 
 
+async fn save_log(log: axum::extract::Json<Log>) -> String {
+    let log = log.0;
+
+    // 构造日志行
+    let log_line = format!("{} - {}: {}\n", log.time, log.client_ip, log.log_content);
+
+    // 打开日志文件，将日志行写入
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("logs.txt")
+        .expect("Failed to open log file");
+
+    file.write_all(log_line.as_bytes())
+        .expect("Failed to write to log file");
+
+    "Log received and saved.".to_string()
+}
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-
-    let json = || async move {
-        let new_user =User{
-            id : 1,
-            username : String::from("lxd"),
-            age : 39,
-        };
-        Ok::<_, std::convert::Infallible>(Json(new_user))
-    };
+    
 
     let app = Router::new()
     .route("/", get(|| async { "Hello, World!" }))
-    .route("/json", get(json));
+    .route("/log", post(save_log()));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
