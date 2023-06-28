4b6645b635d68e4c1d3eb8e3f943ef726d06003e
yuanwai <lxdhzzj@gmail.com>
2023-06-29 01:08:21 +0800

    add axum ip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use axum::{response::IntoResponse, routing::post, Router};
-use hyper::http::Method;
 use axum_client_ip::{InsecureClientIp, SecureClientIp, SecureClientIpSource};
+use hyper::http::Method;
 use serde::Deserialize;
 use std::{fs::OpenOptions, io::Write, net::SocketAddr};
 use tower_http::{
@@ -15,50 +15,68 @@ struct Log {
     log_content: String,
 }
 
-async fn save_log(log: axum::extract::Json<Log>) -> impl IntoResponse {
-    let log = log.0;
-    // 构造日志行
-    let log_line = format!("{} : {} | {}\n", log.time, log.client_ip, log.log_content);
-    println!("============ run app...log:  {}", log_line);
+// async fn save_log(log: axum::extract::Json<Log>, insecure_ip: InsecureClientIp, secure_ip: SecureClientIp) -> impl IntoResponse {
+//     let log = log.0;
 
-    // 打开日志文件，将日志行写入
-    let mut file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("C:\\Users\\win10\\Desktop\\tcs.log")
-        .expect("Failed to open log file");
+//     // 构造日志行
+//     let log_line = format!("{} : {} | {}\n", log.time, log.client_ip, log.log_content);
+//     println!("============ run app...log:  {}", log_line);
 
-    file.write_all(log_line.as_bytes())
-        .expect("Failed to write to log file");
+//     // 打开日志文件，将日志行写入
+//     let mut file = OpenOptions::new()
+//         .create(true)
+//         .append(true)
+//         .open("C:\\Users\\win10\\Desktop\\tcs.log")
+//         .expect("Failed to open log file");
 
-    "Log received and saved.".to_string().into_response()
-}
+//     file.write_all(log_line.as_bytes())
+//         .expect("Failed to write to log file");
 
-
-async fn handler(insecure_ip: InsecureClientIp, secure_ip: SecureClientIp) -> String {
-    format!("{insecure_ip:?} {secure_ip:?}")
-}
+//     "Log received and saved.".to_string().into_response()
+// }
 
 #[tokio::main]
 async fn main() {
+    async fn handler(
+        insecure_ip: InsecureClientIp,
+        secure_ip: SecureClientIp,
+        log: axum::extract::Json<Log>,
+    ) -> String {
+        let log = log.0;
 
-    async fn handler(insecure_ip: InsecureClientIp, secure_ip: SecureClientIp) -> String {
+        let client_ip = format!("{insecure_ip:?}").to_string().s;
+        // 构造日志行
+        let log_line = format!("{} : {} | {}\n", log.time, client_ip, log.log_content);
+        println!("============ run app...log:  {}", log_line);
+
+        // 打开日志文件，将日志行写入
+        let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("C:\\Users\\win10\\Desktop\\tcs.log")
+            .expect("Failed to open log file");
+
+        file.write_all(log_line.as_bytes())
+            .expect("Failed to write to log file");
+
+        "Log received and saved.".to_string().into_response();
         format!("{insecure_ip:?} {secure_ip:?}")
     }
-    
+
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any)
         .allow_headers(Any);
 
     let app = Router::new()
-        .route("/save_log", post(save_log)).layer(SecureClientIpSource::ConnectInfo.into_extension())
+        .route("/save_log", post(handler))
+        .layer(SecureClientIpSource::ConnectInfo.into_extension())
         .layer(AddExtensionLayer::new(()))
         .layer(cors);
+
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .unwrap();
 }
-
